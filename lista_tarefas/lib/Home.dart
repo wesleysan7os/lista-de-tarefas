0c1feea50fc747f0aef91e6dc6aa8c3fbf4c3f62
wesleysan7os <wesley.santos.silva@ccc.ufcg.edu.br>
2021-03-23 23:29:29 -0300

    salvando dados utilizando arquivos

--- lista_tarefas/lib/Home.dart
+++ lista_tarefas/lib/Home.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+import 'dart:async';
+import 'dart:convert';
 
 class Home extends StatefulWidget {
   @override
@@ -7,7 +11,21 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
 
-  List _listaTarefas = ["Ir ao mercado", "estudar", "exercício do dia"];
+  List _listaTarefas = [];
+
+  _salvarArquivo() async {
+    final diretorio = await getApplicationDocumentsDirectory();
+    var arquivo = File("${diretorio.path}/dados.json");
+
+    Map<String, dynamic> tarefa = Map();
+    tarefa["titulo"] = "Ir ao mercado";
+    tarefa["realizada"] = false;
+    _listaTarefas.add(tarefa);
+
+    String dados = json.encode(_listaTarefas);
+    arquivo.writeAsString(dados);
+
+  }
 
   @override
   Widget build(BuildContext context) {
